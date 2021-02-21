95f89beee9c2fd813d4474fe4e35bc37e14a1e1e
Artleksandr <sanyaartamonov2004@mail.ru>
2021-02-21 17:02:26 +1000

    1

---
+++ 1.py
@@ -0,0 +1,53 @@
+import os
+import pygame
+import random
+
+
+def load_image(name, ck=None):
+    a = os.path.join('../data', name)
+    try:
+        image = pygame.image.load(a).convert_alpha()
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if ck is not None:
+        if ck == -1:
+            ck = image.get_at((0, 0))
+        image.set_colorkey(ck)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def main():
+    size = 400, 300
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Кликер говна')
+
+    all_sprites = pygame.sprite.Group()
+
+    enemy = pygame.sprite.Sprite(all_sprites)
+    enemy.image = load_image("creature.png")
+    enemy.rect = enemy.image.get_rect()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and\
+                    enemy.rect.collidepoint(pygame.mouse.get_pos()):
+                print(screen.get_at((0, 0)))
+                pygame.draw.rect(screen, 'black', (enemy.rect[0], enemy.rect[1], enemy.rect[0] + enemy.rect[2],
+                                                   enemy.rect[1] + enemy.rect[3]))
+                enemy.rect.topleft = random.randint(0, size[0] - 82), random.randint(0, size[1] - 100)
+                print('Убил')
+        all_sprites.draw(screen)
+        pygame.display.flip()
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
